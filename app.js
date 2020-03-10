df95c7a2ab8580ba2664402168a8ff5cdfec79db
Inhockorea <ejho13@berkeley.edu>
2020-03-10 16:46:29 -0700

    part 2

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-// For example, here is what the AJAX request URL would look like for search term of “hilarious”: http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym. You can click on this URL and see the JSON you will get back. To view this in a nicer format, we highly recommend using the JSON Viewer chrome extension. This is also the data you should console.log when the form is submitted.
-
-// Our Giphy API key: vsSyKWNnRWpjIrxhNiNYZICrVpLOhqzf
 
 $(function() {
 
+  //Displays a Giphy matching the search term in the user input-box.
+  
   async function getGiphy() {
     event.preventDefault();
 
     let $searchBoxVal = $(".search-term").val();
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${$searchBoxVal}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    let response = await axios.get(
+      `http://api.giphy.com/v1/gifs/search?q=${$searchBoxVal}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+      );
+    
+  //Appends the GIPHY that was randomly generated.
 
-    console.log("response", response);
+    let num = Math.round(Math.random()*10)
+    let embedGiphy = response.data.data[num]["embed_url"]
+    $("#container").append(`<iframe src="${embedGiphy}">`);
   }
 
+  //Eventlistener for the submit button.
+
   $(".button").on("click", getGiphy);
+
+  
 });
 
