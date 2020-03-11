efcffed67a77af773bad419d1a6d1af147f6992d
Brittiany Juravic <britjuravic@Russ-iPhone.localdomain>
2020-03-10 17:17:16 -0700

    Part III compelte

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
+//Displays a Giphy matching the search term in the user input-box.
 
-$(function() {
+async function getGiphy() {
+  event.preventDefault();
 
-  //Displays a Giphy matching the search term in the user input-box.
-  
-  async function getGiphy() {
-    event.preventDefault();
+  let $searchBoxVal = $(".search-term").val();
+  let response = await axios.get(
+    `http://api.giphy.com/v1/gifs/search?q=${$searchBoxVal}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+  );
 
-    let $searchBoxVal = $(".search-term").val();
-    let response = await axios.get(
-      `http://api.giphy.com/v1/gifs/search?q=${$searchBoxVal}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
-      );
-    
-  //Appends the GIPHY that was randomly generated.
+  //Appends the GIPHY that was semi-randomly generated.
 
-    let num = Math.round(Math.random()*10)
-    let embedGiphy = response.data.data[num]["embed_url"]
-    $("#container").append(`<iframe src="${embedGiphy}">`);
-  }
+  let num = Math.round(Math.random() * 10)
+  let embedGiphy = response.data.data[num]["embed_url"]
+  $("#giph-container").append(`<iframe src="${embedGiphy}">`);
+}
 
-  //Eventlistener for the submit button.
+// Removes all displayed giphs from the page.
 
-  $(".button").on("click", getGiphy);
+function removeGiphys() {
+  $("#giph-container").empty();
+}
 
-  
-});
+function waitForDomToLoad() {
+//Eventlistener for the submit and remove buttons.
+  $(".add-btn").on("click", getGiphy);
+  $(".remove-btn").on("click", removeGiphys);
 
+}
+
+$(waitForDomToLoad);
